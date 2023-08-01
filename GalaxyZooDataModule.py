115c03c267e61277af20c94d49c6abb4d3d7d912
Bernd Doser <bernd.doser@h-its.org>
2023-08-01 16:46:39 +0200

    separate GalaxyZooDataModule

---
+++ GalaxyZooDataModule.py
@@ -0,0 +1,38 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+
+import DataSets
+import Preprocessing
+
+
+class GalaxyZooDataModule(pl.LightningDataModule):
+
+    def __init__(self, data_dir: str = "./", batch_size: int = 32, extension: str = "jpg"):
+        super().__init__()
+        self.data_dir = data_dir
+        self.transform = transforms.Compose([
+            Preprocessing.DielemanTransformation(rotation_range=[0,360],
+                translation_range=[4./424,4./424], scaling_range=[1/1.1,1.1], flip=0.5)
+        ])
+        self.batch_size = batch_size
+        self.extension = extension
+
+    def setup(self, stage: str):
+        data_full = DataSets.GalaxyZooDataset(data_directory=self.data_dir,
+        extension=self.extension, transform=self.transform)
+        self.data_train, self.data_val = random_split(data_full, [55000, 6578])
+        self.data_test = data_full
+        self.data_predict = data_full
+
+    def train_dataloader(self):
+        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True, num_workers=16)
+
+    def val_dataloader(self):
+        return DataLoader(self.data_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.data_test, batch_size=self.batch_size)
+
+    def predict_dataloader(self):
+        return DataLoader(self.data_predict, batch_size=self.batch_size)
--- train.py
+++ train.py
@@ -1,52 +1,25 @@
-import io
-import numpy
-from matplotlib import pyplot
-
-import torch
-import torch.linalg
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
-from torch.utils.data import DataLoader
 import pytorch_lightning as pl
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch
+import yaml
 
-from models import RotationalSphericalProjectingAutoencoder
-
-import DataSets
-import Preprocessing
-
-
-class GalaxyZooDataModule(pl.LightningDataModule):
-    def train_dataloader(self):
-        self.dataset = DataSets.GalaxyZooDataset(
-            data_directory="/hits/basement/ain/Data/efigi-1.6/png",
-                #KaggleGalaxyZoo/images_training_rev1",
-                #efigi-1.6/png",
-                #KaggleGalaxyZoo/images_training_rev1",
-            extension=".png",
-            #label_file="/hits/basement/ain/Data/KaggleGalaxyZoo/training_solutions_rev1.csv",
-            transform = transforms.Compose([
-                Preprocessing.DielemanTransformation(rotation_range=[0,360],
-                    translation_range=[4./424,4./424], scaling_range=[1/1.1,1.1], flip=0.5),
-                #Preprocessing.KrizhevskyColorTransformation(
-                    # weights=[-0.0148366, -0.01253134, -0.01040762], std=0.5),
-                Preprocessing.CropAndScale((424,424), (424,424))
-            ])
-        )
-
-        dataloader = DataLoader(self.dataset,
-                                batch_size=64,
-                                shuffle=True,
-                                num_workers=16)
-        return dataloader
+import GalaxyZooDataModule
+import models
 
 if __name__ == "__main__":
-    torch.manual_seed(2345)# 2341 2344
-    data = GalaxyZooDataModule()
-    model = RotationalSphericalProjectingAutoencoder()
-    #checkpoint = torch.load("epoch=2611-step=182840.ckpt")
-    #model.load_state_dict(checkpoint["state_dict"])
-    trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=-1) #accelerator="cpu" devices=1
-    trainer.fit(model, data)
+
+    torch.manual_seed(2345)
+
+    with open('config.yaml', encoding="utf-8") as file:
+        config = yaml.load(file, yaml.SafeLoader)
+
+    # data = GalaxyZooDataModule.GalaxyZooDataModule(
+    #     data_dir = "/hits/basement/ain/Data/efigi-1.6/png")
+    data = GalaxyZooDataModule.GalaxyZooDataModule(
+        data_dir = "/hits/basement/ain/Data/KaggleGalaxyZoo/images_training_rev1", extension="jpg")
+
+    model = models.RotationalSphericalProjectingAutoencoder()
+
+    trainer = pl.Trainer(**config['training'])
+    trainer.fit(model, datamodule=data)
+
+    trainer.test(dataloaders=data)
