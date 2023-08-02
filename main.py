2d780901313433c3a1c8df06b98794096bfac9ed
Bernd Doser <bernd.doser@h-its.org>
2023-08-02 16:01:53 +0200

    overwrite saved config and set matmul precision

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import torch
 from lightning.pytorch.cli import LightningCLI
 
 import GalaxyZooDataModule
 import models
 
+torch.set_float32_matmul_precision('high')
+
 def cli_main():
     cli = LightningCLI(models.RotationalSphericalProjectingAutoencoder,
-                       GalaxyZooDataModule.GalaxyZooDataModule)
+                       GalaxyZooDataModule.GalaxyZooDataModule,
+                       save_config_kwargs={"overwrite": True})
 
 
 if __name__ == "__main__":
