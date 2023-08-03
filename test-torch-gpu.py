66bcf0114939d500f5920fdda65782a046bb4773
Bernd Doser <bernd.doser@h-its.org>
2023-08-03 11:46:24 +0200

    remove test files

--- test-torch-gpu.py
+++
@@ -1,7 +0,0 @@
-import torch
-print("PyTorch version: ", torch.__version__)
-print("CUDA available: ", torch.cuda.is_available())
-print("Number of CUDA devices: ", torch.cuda.device_count())
-
-import torch_xla
-print(torch_xla.__version__)
