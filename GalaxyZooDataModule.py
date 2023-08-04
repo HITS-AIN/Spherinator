2831020e8128bf60c0a5d793afca6aa1b99c28e3
kai-polsterer <kai.polsterer@h-its.org>
2023-08-04 17:08:35 +0200

    removed splitting of data and some transforms

--- GalaxyZooDataModule.py
+++ GalaxyZooDataModule.py
@@ -1,22 +1,24 @@
 import lightning.pytorch as pl
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchvision import transforms
 
 import DataSets
 import Preprocessing
-
 
 class GalaxyZooDataModule(pl.LightningDataModule):
 
     def __init__(self, data_dir: str = "./", batch_size: int = 32, extension: str = "jpg"):
         super().__init__()
         self.data_dir = data_dir
-        self.transform = transforms.Compose([
-            Preprocessing.DielemanTransformation(
+        self.train_transform = transforms.Compose([
+            Preprocessing.DielemanTransformation( # TODO: in config file?
                 rotation_range=[0,360],
                 translation_range=[4./424,4./424],
                 scaling_range=[1/1.1,1.1],
                 flip=0.5),
+            Preprocessing.CropAndScale((424,424), (424,424))
+        ])
+        self.val_transform = transforms.Compose([
             Preprocessing.CropAndScale((424,424), (424,424))
         ])
         self.batch_size = batch_size
@@ -24,13 +26,18 @@ class GalaxyZooDataModule(pl.LightningDataModule):
 
     def setup(self, stage: str):
         data_full = DataSets.GalaxyZooDataset(data_directory=self.data_dir,
-        extension=self.extension, transform=self.transform)
-        self.data_train, self.data_val = random_split(data_full, [55000, 6578])
+                                              extension=self.extension,
+                                              transform=self.train_transform)
+        data_raw = DataSets.GalaxyZooDataset(data_directory=self.data_dir,
+                                             extension=self.extension,
+                                             transform=self.val_transform)
+        self.data_train = data_full
+        self.data_val = data_raw
         self.data_test = data_full
         self.data_predict = data_full
 
     def train_dataloader(self):
-        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True, num_workers=16)
+        return DataLoader(self.data_train, batch_size=self.batch_size, shuffle=True, num_workers=16) #TODO: move this to the config
 
     def val_dataloader(self):
         return DataLoader(self.data_val, batch_size=self.batch_size)
