51bbbee60a4ad4b0427b3c07af332a2eb30149f8
kai-polsterer <kai.polsterer@h-its.org>
2023-09-07 23:55:42 +0200

    illustris meta data extraction

--- data/illustris_sdss_dataset.py
+++ data/illustris_sdss_dataset.py
@@ -29,13 +29,22 @@ class IllustrisSdssDataset(Dataset):
         self.data_directories = data_directories
         self.transform = transform
         self.files = []
+        self.metadata = []
+        self.total_files = 0
         for data_directory in data_directories:
             for file in os.listdir(data_directory):
                 if file.endswith(extension):
+                    self.total_files = self.total_files + 1
                     fits_filename = os.path.join(data_directory, file)
                     size = fits.getval(fits_filename, "NAXIS1")
                     if int(size) >= minsize:
                         self.files.append(fits_filename)
+                        info = {}
+                        splits = fits_filename[:-(len(extension))].split('/')
+                        info['simulation'] = splits[-5]
+                        info['snapshot'] = splits[-3].split('_')[1]
+                        info['subhalo_id'] = splits[-1].split('_')[1]
+                        self.metadata.append(info)
         self.len = len(self.files)
 
     def __len__(self):
@@ -60,7 +69,7 @@ class IllustrisSdssDataset(Dataset):
         data = fits.getdata(self.files[idx], 0)
         data = numpy.array(data).astype(numpy.float32)
         image = torch.Tensor(data)
-        sample = {'image': image, 'filename': self.files[idx], 'id': idx}
+        sample = {'image': image, 'filename': self.files[idx], 'id': idx, 'metadata': self.metadata[idx]}
         if self.transform:
             sample = self.transform(sample)
         return sample
