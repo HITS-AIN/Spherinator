229d9654fd648bd1e91d71ea2e959319bd0149da
Bernd Doser <bernd.doser@h-its.org>
2023-09-26 18:29:07 +0200

    remove old models and scripts

--- models/__init__.py
+++ models/__init__.py
@@ -8,16 +8,16 @@ It contains the following modules:
     A plain convolutional autoencoder projecting on a sphere with naive rotation invariance.
 2. `rotational_spherical_variational_autoencoder`:
     A convolutional variational autoencoder projecting on a sphere with naive rotation invariance.
+3. `SVAE`:
+    Straightforward implementation of hyperspherical variational autoencoder (SVAE).
 """
 
 from .rotational_autoencoder import RotationalAutoencoder
 from .rotational_variational_autoencoder import RotationalVariationalAutoencoder
 from .svae import SVAE
-from .vae import VAE
 
 __all__ = [
     'RotationalAutoencoder',
     'RotationalVariationalAutoencoder',
     'SVAE',
-    'VAE',
 ]
