bdc48422191ca3d2288ab8685105eafbeabeb3ec
Bernd Doser <bernd.doser@h-its.org>
2023-10-18 11:52:49 +0200

    Raise a FloatingPointError exception

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 """ Uses the command line client to start the training.
 """
 
+import numpy as np
 import torch
 from lightning.pytorch.cli import LightningCLI
 
+# Raise a FloatingPointError for any kind of floating-point errors
+if __debug__:
+    np.seterr(all='raise')
+    print('debug on')
+else:
+    print('debug off')
+
+# Set the default precision of torch operations to float32
 torch.set_float32_matmul_precision('high')
 
 if __name__ == "__main__":
