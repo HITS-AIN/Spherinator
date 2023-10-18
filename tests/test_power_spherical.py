b44dc623216a4864bff6d392321aab32b05b57dc
Bernd Doser <bernd.doser@h-its.org>
2023-10-18 11:51:51 +0200

    test power spherical 2d batch

--- tests/test_power_spherical.py
+++ tests/test_power_spherical.py
@@ -12,5 +12,17 @@ def test_power_spherical_2d():
     dist = PowerSpherical(torch.Tensor([0.0, 0.0]), torch.Tensor([1.0]))
 
     assert dist.has_rsample == True
-    assert torch.isclose(dist.sample(), torch.Tensor([-0.9971,  0.0766]), rtol = 1e-3).all()
-    assert torch.isclose(dist.sample(), torch.Tensor([-0.5954,  0.8034]), rtol = 1e-3).all()
+    sample = dist.rsample()
+    assert sample.shape == torch.Size([2])
+    assert torch.isclose(sample, torch.Tensor([-0.9971,  0.0766]), rtol = 1e-3).all()
+    assert torch.isclose(dist.rsample(), torch.Tensor([-0.5954,  0.8034]), rtol = 1e-3).all()
+
+def test_power_spherical_2d_batch():
+    torch.manual_seed(0)
+    batch_size = 32
+    loc = torch.randn(batch_size, 3)
+    scale = torch.ones(batch_size)
+    dist = PowerSpherical(loc, scale)
+
+    sample = dist.rsample()
+    assert sample.shape == torch.Size([batch_size, 3])
