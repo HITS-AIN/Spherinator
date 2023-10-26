7c18deb813800bbb08913855c55ebe4dcf0f1506
Bernd Doser <bernd.doser@h-its.org>
2023-10-26 17:39:22 +0200

    work on kl divergence

--- models/rotational_variational_autoencoder_power.py
+++ models/rotational_variational_autoencoder_power.py
@@ -95,12 +95,13 @@ class RotationalVariationalAutoencoderPower(SpherinatorModule):
         x = F.relu(self.conv4(x))
         x = self.pool4(x)
         x = x.view(-1, 256*4*4)
-        x = F.tanh(self.fc1(x))
+        x = F.sigmoid(self.fc1(x))
 
         z_mean = self.fc_mean(x)
         z_mean = torch.nn.functional.normalize(z_mean, p=2.0, dim=1)
         # SVAE code: the `+ 1` prevent collapsing behaviors
-        z_var = F.softplus(self.fc_var(x)) + 1
+        # z_var = F.softplus(self.fc_var(x)) + 0.1
+        z_var = torch.exp(self.fc_var(x)) + 20.0
 
         return z_mean, z_var
 
@@ -115,7 +116,7 @@ class RotationalVariationalAutoencoderPower(SpherinatorModule):
         x = F.relu(self.deconv4(x))
         x = F.relu(self.deconv5(x))
         x = self.deconv6(x)
-        x = torch.sigmoid(x)
+        # x = torch.sigmoid(x)
         return x
 
     def reparameterize(self, z_mean, z_var):
@@ -135,12 +136,14 @@ class RotationalVariationalAutoencoderPower(SpherinatorModule):
         losses = torch.zeros(images.shape[0], self.rotations)
         losses_recon = torch.zeros(images.shape[0], self.rotations)
         losses_KL = torch.zeros(images.shape[0], self.rotations)
+        z_mean = torch.zeros(self.z_dim)
+        z_scale = torch.zeros(self.z_dim)
         for i in range(self.rotations):
             rotate = functional.rotate(images, 360.0 / self.rotations * i, expand=False)
             crop = functional.center_crop(rotate, [self.crop_size, self.crop_size])
             scaled = functional.resize(crop, [self.input_size, self.input_size], antialias=False)
 
-            (_, _), (q_z, p_z), _, recon = self.forward(scaled)
+            (z_mean, z_scale), (q_z, p_z), _, recon = self.forward(scaled)
 
             loss_recon = self.reconstruction_loss(scaled, recon)
             loss_KL = torch.distributions.kl.kl_divergence(q_z, p_z)
@@ -157,6 +160,8 @@ class RotationalVariationalAutoencoderPower(SpherinatorModule):
         self.log('loss_recon', loss_recon, prog_bar=True)
         self.log('loss_KL', loss_KL)
         self.log('learning_rate', self.optimizers().param_groups[0]['lr'])
+        self.log('mean(z_mean) ', torch.mean(z_mean))
+        self.log('mean(z_scale) ', torch.mean(z_scale))
         return loss
 
     def configure_optimizers(self):
@@ -171,9 +176,9 @@ class RotationalVariationalAutoencoderPower(SpherinatorModule):
         return self.decode(coordinates)
 
     def reconstruction_loss(self, images, reconstructions):
-        # return torch.sqrt(nn.MSELoss(reduction='none')(
-        #     reconstructions.reshape(-1, self.total_input_size),
-        #     images.reshape(-1, self.total_input_size)).mean(-1))
-        return nn.MSELoss(reduction='none')(
+        return torch.sqrt(nn.MSELoss(reduction='none')(
             reconstructions.reshape(-1, self.total_input_size),
-            images.reshape(-1, self.total_input_size)).sum(-1)
+            images.reshape(-1, self.total_input_size)).mean(-1))
+        # return nn.MSELoss(reduction='none')(
+        #     reconstructions.reshape(-1, self.total_input_size),
+        #     images.reshape(-1, self.total_input_size)).sum(-1)
