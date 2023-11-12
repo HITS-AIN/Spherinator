fix normalization of shapes dataset

data/shapes_dataset.py
@@ -32,8 +32,9 @@ class ShapesDataset(Dataset):
 
         self.images = np.empty((0,64,64), np.float32)
         for file in os.listdir(data_directory):
-            self.images = np.append(self.images, np.load(os.path.join(data_directory, file)),
-                axis=0).astype(np.float32)
+            image = np.load(os.path.join(data_directory, file)).astype(np.float32)
+            image = (image - np.min(image)) / (np.max(image) - np.min(image)) # Normalize
+            self.images = np.append(self.images, image, axis=0)
 
     def __len__(self):
         """ Return the number of items in the dataset.

tests/test_illustris_sdss_data_module.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from data import IllustrisSdssDataModule
 
@@ -28,4 +29,6 @@ def test_fits():
 
     assert batch["image"].shape == (1, 3, 363, 363)
     assert batch["image"].dtype == torch.float32
-    assert batch["image"].max() == 1.0
+
+    assert np.isclose(batch["image"].min(), 0.0)
+    assert np.isclose(batch["image"].max(), 1.0)

tests/test_shapes_data_module.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from data import ShapesDataModule
 
@@ -8,10 +9,18 @@ def test_fit():
     data = ShapesDataModule("tests/data/shapes", num_workers=1)
     data.setup("fit")
 
-    assert data.train_dataloader().batch_size == 32
+    assert len(data.data_train) == 4000
 
-    batch = next(iter(data.train_dataloader()))
+    dataloader = data.train_dataloader()
+
+    assert dataloader.batch_size == 32
+    assert len(dataloader) == len(data.data_train) / dataloader.batch_size
+    assert dataloader.num_workers == 1
+
+    batch = next(iter(dataloader))
 
     assert batch["image"].shape == (32, 3, 91, 91)
     assert batch["image"].dtype == torch.float32
-    assert batch["image"].max() == 0.9668
+
+    assert np.isclose(batch["image"].min(), 0.0, atol = 1e-2)
+    assert np.isclose(batch["image"].max(), 1.0, atol = 1e-2)