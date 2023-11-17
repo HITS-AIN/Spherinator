73ea8b924e414b8d473f82d2457d968e0c60b3fe
Bernd Doser <bernd.doser@h-its.org>
2023-11-17 12:17:59 +0100

    exclude files in shapes dataset

--- data/shapes_data_module.py
+++ data/shapes_data_module.py
@@ -11,19 +11,23 @@ from data.shapes_dataset import ShapesDataset
 
 
 class ShapesDataModule(pl.LightningDataModule):
-    """ Defines access to the ShapesDataset.
-    """
-    def __init__(self,
-                 data_directory: str,
-                 shuffle: bool = True,
-                 image_size: int = 91,
-                 batch_size: int = 32,
-                 num_workers: int = 1,
-                 download: bool = False):
-        """ Initializes the data loader
+    """Defines access to the ShapesDataset."""
+
+    def __init__(
+        self,
+        data_directory: str,
+        exclude_files: List[str] | str = [],
+        shuffle: bool = True,
+        image_size: int = 91,
+        batch_size: int = 32,
+        num_workers: int = 1,
+        download: bool = False,
+    ):
+        """Initializes the data loader
 
         Args:
             data_directories (List[str]): The data directory
+            exclude_files (List[str], optional): A list of files to exclude. Defaults to [].
             shuffle (bool, optional): Wether or not to shuffle whe reading. Defaults to True.
             image_size (int, optional): The size of the images. Defaults to 91.
             batch_size (int, optional): The batch size for training. Defaults to 32.
@@ -33,17 +37,22 @@ class ShapesDataModule(pl.LightningDataModule):
         super().__init__()
 
         self.data_directory = data_directory
+        self.exclude_files = exclude_files
         self.shuffle = shuffle
         self.image_size = image_size
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.download = download
 
-        self.transform_train = transforms.Compose([
-            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
-            transforms.Resize((self.image_size, self.image_size), antialias="none"),
-            transforms.Lambda(lambda x: (x - torch.min(x)) / (torch.max(x) - torch.min(x))),
-        ])
+        self.transform_train = transforms.Compose(
+            [
+                transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+                transforms.Resize((self.image_size, self.image_size), antialias="none"),
+                transforms.Lambda(  # Normalize
+                    lambda x: (x - torch.min(x)) / (torch.max(x) - torch.min(x))
+                ),
+            ]
+        )
         self.transform_predict = self.transform_train
         self.transform_val = self.transform_train
 
@@ -55,43 +64,59 @@ class ShapesDataModule(pl.LightningDataModule):
         self.dataloader_val = None
 
     def setup(self, stage: str):
-        """ Sets up the data set and data loaders.
+        """Sets up the data set and data loaders.
 
         Args:
             stage (str): Defines for which stage the data is needed.
                          For the moment just fitting is supported.
         """
         if stage == "fit":
-            self.data_train = ShapesDataset(data_directory=self.data_directory,
-                                            transform=self.transform_train,
-                                            download=self.download)
+            self.data_train = ShapesDataset(
+                data_directory=self.data_directory,
+                exclude_files=self.exclude_files,
+                transform=self.transform_train,
+                download=self.download,
+            )
 
-            self.dataloader_train = DataLoader(self.data_train,
-                                              batch_size=self.batch_size,
-                                              shuffle=self.shuffle,
-                                              num_workers=self.num_workers)
+            self.dataloader_train = DataLoader(
+                self.data_train,
+                batch_size=self.batch_size,
+                shuffle=self.shuffle,
+                num_workers=self.num_workers,
+            )
+
         if stage == "predict":
-            self.data_predict = ShapesDataset(data_directory=self.data_directory,
-                                              transform=self.transform_predict,
-                                              download=self.download)
+            self.data_predict = ShapesDataset(
+                data_directory=self.data_directory,
+                exclude_files=self.exclude_files,
+                transform=self.transform_predict,
+                download=self.download,
+            )
 
-            self.dataloader_predict = DataLoader(self.data_predict,
-                                                 batch_size=self.batch_size,
-                                                 shuffle=False,
-                                                 num_workers=self.num_workers)
+            self.dataloader_predict = DataLoader(
+                self.data_predict,
+                batch_size=self.batch_size,
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
 
         if stage == "val":
-            self.data_val = ShapesDataset(data_directory=self.data_directory,
-                                          transform=self.transform_val,
-                                          download=self.download)
+            self.data_val = ShapesDataset(
+                data_directory=self.data_directory,
+                exclude_files=self.exclude_files,
+                transform=self.transform_val,
+                download=self.download,
+            )
 
-            self.dataloader_val = DataLoader(self.data_val,
-                                             batch_size=self.batch_size,
-                                             shuffle=False,
-                                             num_workers=self.num_workers)
+            self.dataloader_val = DataLoader(
+                self.data_val,
+                batch_size=self.batch_size,
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
 
     def train_dataloader(self):
-        """ Gets the data loader for training.
+        """Gets the data loader for training.
 
         Returns:
             torch.utils.data.DataLoader: The dataloader instance to use for training.
@@ -99,7 +124,7 @@ class ShapesDataModule(pl.LightningDataModule):
         return self.dataloader_train
 
     def predict_dataloader(self):
-        """ Gets the data loader for prediction.
+        """Gets the data loader for prediction.
 
         Returns:
             torch.utils.data.DataLoader: The dataloader instance to use for prediction.
@@ -107,7 +132,7 @@ class ShapesDataModule(pl.LightningDataModule):
         return self.dataloader_predict
 
     def val_dataloader(self):
-        """ Gets the data loader for validation.
+        """Gets the data loader for validation.
 
         Returns:
             torch.utils.data.DataLoader: The dataloader instance to use for validation.
