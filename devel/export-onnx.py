b557b2ddcc815128885ea67ba4404e41ca4657a1
Bernd Doser <bernd.doser@h-its.org>
2023-11-22 17:48:23 +0100

    test export models

--- devel/export-onnx.py
+++ devel/export-onnx.py
@@ -6,10 +6,11 @@ sys.path.append(os.path.join(script_dir, '../'))
 
 import models
 
-model = models.RotationalVariationalAutoencoder()
+# model = models.RotationalVariationalAutoencoder()
+model = models.RotationalVariationalAutoencoderPower()
 
 # Test the model with a dummy input
 model(model.example_input_array)
 
-filepath = "RotationalVAE.onnx"
+filepath = "model.onnx"
 model.to_onnx(filepath, export_params=True, verbose=True)
