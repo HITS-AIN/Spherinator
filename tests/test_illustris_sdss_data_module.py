0c5d5450603a1196f4cc48543b57ebd2c0b23b04
Bernd Doser <bernd.doser@h-its.org>
2023-11-27 16:09:26 +0100

    more fits for testing IllustrisSdssDataModule

--- tests/test_illustris_sdss_data_module.py
+++ tests/test_illustris_sdss_data_module.py
@@ -17,22 +17,31 @@ def test_empty():
 
 
 def test_fits():
+    torch.manual_seed(0)
     data = IllustrisSdssDataModule(
         ["tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/"],
-        num_workers=1,
+        num_workers=4,
+        batch_size=4,
+        shuffle=True,
     )
     data.setup("fit")
 
     dataloader = data.train_dataloader()
-    assert dataloader.batch_size == 32
+    assert dataloader.batch_size == 4
 
     batch = next(iter(dataloader))
 
-    assert batch["image"].shape == (1, 3, 363, 363)
+    assert batch["image"].shape == (4, 3, 363, 363)
     assert batch["image"].dtype == torch.float32
+
+    print(batch["filename"])
+
     assert batch["filename"] == [
-        "tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/broadband_99185.fits"
+        "tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/broadband_117392.fits",
+        "tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/broadband_117369.fits",
+        "tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/broadband_117390.fits",
+        "tests/data/SKIRT_synthetic_images/TNG50/sdss/snapnum_095/data/broadband_117384.fits",
     ]
 
-    assert np.isclose(batch["image"].min(), 0.0)
-    assert np.isclose(batch["image"].max(), 1.0)
+    assert batch["image"].min() >= 0.0
+    assert batch["image"].max() <= 1.0
