648dad1694fe587ddfd1421dd8a4aeb944dc8cbc
Bernd Doser <bernd.doser@h-its.org>
2023-12-07 10:11:51 +0100

    add shebang to spherinator file

--- spherinator.py
+++ spherinator.py
@@ -1,5 +1,5 @@
-""" Uses the command line client to start the training.
-"""
+#!/usr/bin/env python3
+""" Uses the command line client to start the training."""
 
 import numpy as np
 import torch
@@ -7,14 +7,14 @@ from lightning.pytorch.cli import LightningCLI
 
 # Raise a FloatingPointError for any kind of floating-point errors
 if __debug__:
-    np.seterr(all='raise')
-    print('debug on')
+    np.seterr(all="raise")
+    print("debug on")
 else:
-    print('debug off')
+    print("debug off")
 
 # Set the default precision of torch operations to float32
 # See https://github.com/Lightning-AI/lightning/discussions/16698
-torch.set_float32_matmul_precision('high')
+torch.set_float32_matmul_precision("high")
 
 if __name__ == "__main__":
     cli = LightningCLI(save_config_kwargs={"overwrite": True})
