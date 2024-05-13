546f7fca8ab8aa56c5c6eee950a34adfc31c601e
Bernd Doser <bernd.doser@h-its.org>
2024-05-13 16:48:41 +0200

    remove recommonmark extension

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ project = "Spherinator"
 copyright = f"{datetime.now().year}, HITS gGmbH"
 author = "Kai Polsterer <kai.polsterer@h-its.org>, Bernd Doser <bernd.doser@h-its.org>, Andreas Fehlner <andreas.fehlner@h-its.org>, Sebastian T. Gomez <sebastian.trujillogomez@h-its.org>"
 extensions = [
-    "recommonmark",
+    # "recommonmark",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
