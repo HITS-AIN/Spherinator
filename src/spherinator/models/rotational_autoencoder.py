cc27bf7b69ea9b5f8679c6bba3ed0d0b465bd7f8
Bernd Doser <bernd.doser@h-its.org>
2024-05-24 18:57:28 +0200

    update plain autoencoder

--- src/spherinator/models/rotational_autoencoder.py
+++ src/spherinator/models/rotational_autoencoder.py
@@ -4,137 +4,75 @@ import torch
 import torch.linalg
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.transforms.functional as functional
+import torchvision.transforms.v2.functional as functional
 from torch.optim import Adam
 
+from .convolutional_decoder import ConvolutionalDecoder
+from .convolutional_encoder import ConvolutionalEncoder
 from .spherinator_module import SpherinatorModule
 
 
 class RotationalAutoencoder(SpherinatorModule):
-    def __init__(self, image_size: int = 363, rotations: int = 36, bottleneck: int = 3):
-        """
-        RotationalAutoencoder initializer
+    def __init__(
+        self,
+        encoder: nn.Module = ConvolutionalEncoder(),
+        decoder: nn.Module = ConvolutionalDecoder(),
+        z_dim: int = 2,
+        image_size: int = 91,
+        input_size: int = 128,
+        rotations: int = 36,
+    ):
+        """Initializer
 
-        :param image_size: size of the input images
-        :param rotations: number of rotations
-        :param beta: factor for beta-VAE
+        Args:
+            z_dim (int, optional): dimension of the latent representation. Defaults to 2.
+            image_size (int, optional): size of the input images. Defaults to 91.
+            rotations (int, optional): number of rotations. Defaults to 36.
         """
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters(ignore=["encoder", "decoder"])
 
+        self.encoder = encoder
+        self.decoder = decoder
+        self.z_dim = z_dim
         self.image_size = image_size
+        self.input_size = input_size
         self.rotations = rotations
-        self.bottleneck = bottleneck
 
         self.crop_size = int(self.image_size * math.sqrt(2) / 2)
-        self.input_size = 128
+        self.total_input_size = self.input_size * self.input_size * 3
 
-        self.example_input_array = torch.randn(
-            1, bottleneck, self.input_size, self.input_size
-        )
-
-        self.conv0 = nn.Conv2d(
-            in_channels=3, out_channels=16, kernel_size=(3, 3), stride=1, padding=1
-        )  # 128x128
-        self.pool0 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)  # 64x64
-        self.conv1 = nn.Conv2d(
-            in_channels=16, out_channels=32, kernel_size=(3, 3), stride=1, padding=1
-        )  # 64x64
-        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)  # 32x32
-        self.conv2 = nn.Conv2d(
-            in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1, padding=1
-        )  # 32x32
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)  # 16x16
-        self.conv3 = nn.Conv2d(
-            in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding=1
-        )  # 16x16
-        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)  # 8x8
-        self.conv4 = nn.Conv2d(
-            in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding=1
-        )  # 8x8
-        self.pool4 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)  # 4x4
-
-        self.fc1 = nn.Linear(256 * 4 * 4, 256)
-        self.fc2 = nn.Linear(256, self.bottleneck)
-        self.fc3 = nn.Linear(self.bottleneck, 256)
-        self.fc4 = nn.Linear(256, 256 * 4 * 4)
-
-        self.deconv1 = nn.ConvTranspose2d(
-            in_channels=256, out_channels=128, kernel_size=(4, 4), stride=2, padding=1
-        )  # 8x8
-        self.deconv2 = nn.ConvTranspose2d(
-            in_channels=128, out_channels=128, kernel_size=(4, 4), stride=2, padding=1
-        )  # 16x16
-        self.deconv3 = nn.ConvTranspose2d(
-            in_channels=128, out_channels=64, kernel_size=(4, 4), stride=2, padding=1
-        )  # 32x32
-        self.deconv4 = nn.ConvTranspose2d(
-            in_channels=64, out_channels=32, kernel_size=(4, 4), stride=2, padding=1
-        )  # 64x64
-        self.deconv5 = nn.ConvTranspose2d(
-            in_channels=32, out_channels=16, kernel_size=(3, 3), stride=2, padding=1
-        )  # 127x127
-        self.deconv6 = nn.ConvTranspose2d(
-            in_channels=16, out_channels=3, kernel_size=(2, 2), stride=1, padding=0
-        )  # 128x128
+        self.example_input_array = torch.randn(1, 3, self.input_size, self.input_size)
 
     def get_input_size(self):
         return self.input_size
 
     def encode(self, x):
-        x = F.relu(self.conv0(x))
-        x = self.pool0(x)
-        x = F.relu(self.conv1(x))
-        x = self.pool1(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool2(x)
-        x = F.relu(self.conv3(x))
-        x = self.pool3(x)
-        x = F.relu(self.conv4(x))
-        x = self.pool4(x)
-        x = x.view(-1, 256 * 4 * 4)
-        x = F.tanh(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        z = self.encoder(x)
+        return z
 
-    def scale_to_unity(self, x):
-        length = torch.linalg.vector_norm(x, dim=1) + 1.0e-20
-        return (x.T / length).T
-
-    def decode(self, x):
-        x = F.tanh(self.fc3(x))
-        x = F.tanh(self.fc4(x))
-        x = x.view(-1, 256, 4, 4)
-        x = F.relu(self.deconv1(x))
-        x = F.relu(self.deconv2(x))
-        x = F.relu(self.deconv3(x))
-        x = F.relu(self.deconv4(x))
-        x = F.relu(self.deconv5(x))
-        x = self.deconv6(x)
+    def decode(self, z):
+        x = self.decoder(z)
         return x
 
     def forward(self, x):
-        coordinates = self.encode(x)
-        reconstruction = self.decode(self.scale_to_unity(coordinates))
-        return reconstruction, coordinates
-
-    def spherical_loss(self, images, reconstructions, coordinates):
-        coord_regularization = torch.square(
-            1 - torch.sum(torch.square(coordinates), dim=1)
-        )
-        reconstruction_loss = self.reconstruction_loss(images, reconstructions)
-        loss = reconstruction_loss + 1e-4 * coord_regularization
-        return loss
+        z = self.encode(x)
+        recon = self.decode(z)
+        return recon
 
     def training_step(self, batch, batch_idx):
-        best_recon = torch.ones(batch.shape[0], device=batch.device) * 1e10
-        best_scaled = torch.zeros(
-            (batch.shape[0], batch.shape[1], self.input_size, self.input_size),
-            device=batch.device,
-        )
 
         with torch.no_grad():
-            for i in range(self.rotations):
+            crop = functional.center_crop(batch, [self.crop_size, self.crop_size])
+            scaled = functional.resize(
+                crop, [self.input_size, self.input_size], antialias=True
+            )
+
+        recon = self.forward(scaled)
+        loss = self.reconstruction_loss(scaled, recon)
+
+        for i in range(1, self.rotations):
+            with torch.no_grad():
                 rotate = functional.rotate(
                     batch, 360.0 / self.rotations * i, expand=False
                 )
@@ -143,17 +81,11 @@ class RotationalAutoencoder(SpherinatorModule):
                     crop, [self.input_size, self.input_size], antialias=True
                 )
 
-                recon, _ = self.forward(scaled)
-                loss_recon = self.reconstruction_loss(scaled, recon)
-                best_recon_idx = torch.where(loss_recon < best_recon)
-                best_recon[best_recon_idx] = loss_recon[best_recon_idx]
-                best_scaled[best_recon_idx] = scaled[best_recon_idx]
+            loss = torch.min(loss, self.reconstruction_loss(scaled, recon))
 
-        recon, coord = self.forward(best_scaled)
+        loss = loss.mean()
 
-        loss = self.spherical_loss(best_scaled, recon, coord).mean()
-
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, prog_bar=True)
         self.log("learning_rate", self.optimizers().param_groups[0]["lr"])
         return loss
 
@@ -162,20 +94,16 @@ class RotationalAutoencoder(SpherinatorModule):
         return Adam(self.parameters(), lr=1e-3)
 
     def project(self, images):
-        return self.scale_to_unity(self.encode(images))
+        z = self.encode(images)
+        return z
 
     def reconstruct(self, coordinates):
         return self.decode(coordinates)
 
     def reconstruction_loss(self, images, reconstructions):
         return torch.sqrt(
-            torch.mean(
-                torch.square(
-                    images.reshape(-1, 3 * images.shape[-2] * images.shape[-1])
-                    - reconstructions.reshape(
-                        -1, 3 * images.shape[-2] * images.shape[-1]
-                    )
-                ),
-                dim=-1,
-            )
+            nn.MSELoss(reduction="none")(
+                reconstructions.reshape(-1, self.total_input_size),
+                images.reshape(-1, self.total_input_size),
+            ).mean(dim=1)
         )
