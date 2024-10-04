e5e98681a207b4fc17f5fc3ff52e665dd5527b28
Bernd Doser <bernd.doser@h-its.org>
2024-10-04 20:10:20 +0200

    litdata optimize

--- parquet/litdata_optimize.py
+++ parquet/litdata_optimize.py
@@ -4,14 +4,14 @@ import os
 import pyarrow.parquet as pq
 from lightning.data import optimize
 from PIL import Image
+from sympy import root
 
-# 1. List the parquet files
-input_dir = f"/teamspace/studios/this_studio/dataframe_data"
-output_dir = "/teamspace/datasets/lightning_data"
+root_dir = "/hits/fast/its/doserbd/data/machine-learning/SKIRT_synthetic_images"
+input_dir = root_dir + "/parquet"
+output_dir = root_dir + "lightning"
 parquet_files = sorted([os.path.join(input_dir, f) for f in os.listdir(input_dir)])
 
 
-# 2. Define the function to read parquet files and yield the samples
 def convert_parquet_to_lightning_data(parquet_file):
     parquet_file = pq.ParquetFile(parquet_file)
     # Process per batch to reduce RAM usage
@@ -19,20 +19,15 @@ def convert_parquet_to_lightning_data(parquet_file):
         df = batch.to_pandas()
         df["image"] = df["image"].apply(lambda x: Image.open(io.BytesIO(x)))
         for row in df.itertuples():
-            sample = (
-                row.id,
-                row.timestamp.strftime("%m/%d/%Y %I:%M %p"),
-                row.description,
-                row.image,
-            )
-            yield sample  # -> encode the sample into binary chunks
+            sample = (row.image,)
+            yield sample
 
 
-# 3. Apply the optimize operator over the parquet files
-optimize(
-    convert_parquet_to_lightning_data,
-    parquet_files[:10],
-    output_dir,
-    num_workers=os.cpu_count(),
-    chunk_bytes="64MB",
-)
+if __name__ == "__main__":
+    optimize(
+        convert_parquet_to_lightning_data,
+        parquet_files[:10],
+        output_dir,
+        num_workers=1,
+        chunk_bytes="64MB",
+    )
