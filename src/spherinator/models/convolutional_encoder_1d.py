57ec201b0139c4afb9b32d4adc491b5115c11ed9
Bernd Doser <bernd.doser@h-its.org>
2025-02-10 16:52:12 +0100

    revert ConvolutionalEncoder1D

--- src/spherinator/models/convolutional_encoder_1d.py
+++ src/spherinator/models/convolutional_encoder_1d.py
@@ -1,81 +1,39 @@
-from typing import Callable, Optional
-
 import torch
 import torch.nn as nn
 
 
 class ConvolutionalEncoder1D(nn.Module):
-    def __init__(
-        self,
-        input_dim: int,
-        output_dim: int,
-        in_channels: int = 1,
-        inc_channels: int = 3,
-        kernel_size: int = 3,
-        number_of_layers: int = 1,
-        striding: int = 1,
-        padding: int = 0,
-        activation: Optional[Callable[..., nn.Module]] = nn.ReLU,
-        norm: Optional[Callable[..., nn.Module]] = nn.BatchNorm1d,
-        pooling: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
+    def __init__(self, input_dim: int, output_dim: int):
         """ConvolutionalEncoder1D initializer
-        Args:
-            input_dim (int): The number of input features
-            output_dim (int): The number of output features
-            kernel_size (int, optional): The kernel size. Defaults to 5.
-            number_of_layers (int, optional): The number of layers. Defaults to 3.
-            striding (int, optional): The stride. Defaults to 1.
-            padding (int, optional): The padding. Defaults to 0.
-            activation (Optional[Callable[..., nn.Module]], optional): The activation function.
-            Defaults to nn.ReLU.
-            norm (Optional[Callable[..., nn.Module]], optional): The normalization layer.
-            Defaults to nn.BatchNorm1d.
-            pooling (Optional[Callable[..., nn.Module]], optional): The pooling layer.
-            Defaults to None.
+        Input shape: (batch_size, 1, input_dim)
+        Output shape: (batch_size, output_dim)
         """
         super().__init__()
 
+        assert input_dim % 4 == 0, "input_dim must be divisible by 4"
+
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.kernel_size = kernel_size
-        self.number_of_layers = number_of_layers
-        self.striding = striding
-        self.padding = padding
-        self.activation = activation
-        self.norm = norm
-        self.pooling = pooling
 
-        self.example_input_array = torch.randn(1, 1, input_dim)
+        self.example_input_array = torch.randn(2, 1, input_dim)
 
-        layers = []
-        nb_channels = in_channels
-        for _ in range(number_of_layers):
-            out_channels = nb_channels + inc_channels
-            layers.append(
-                nn.Conv1d(
-                    nb_channels,
-                    out_channels,
-                    kernel_size=kernel_size,
-                    stride=striding,
-                    padding=padding,
-                )
-            )
-            if norm is not None:
-                layers.append(norm(out_channels))
-            if activation is not None:
-                layers.append(activation())
-            if pooling is not None:
-                layers.append(pooling())
-            nb_channels = out_channels
-        self.cnn = nn.Sequential(*layers)
-
-        self.fc = nn.Sequential(
+        self.enc1 = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+        )  # 32 x (input_dim / 2)
+        self.enc2 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+        )  # 64 x (input_dim / 4)
+        self.enc3 = nn.Sequential(
             nn.Flatten(),
-            nn.LazyLinear(output_dim),
+            nn.Linear(int(64 * input_dim / 4), output_dim),
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.cnn(x)
-        x = self.fc(x)
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        x = self.enc1(x)
+        x = self.enc2(x)
+        x = self.enc3(x)
         return x
