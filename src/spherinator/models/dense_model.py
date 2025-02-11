b8aca51599d8ada169cfa0c71958d9a0c1031ce8
Bernd Doser <bernd.doser@h-its.org>
2025-02-11 22:24:25 +0100

    Add Flatten layer to DenseModel for improved input handling

--- src/spherinator/models/dense_model.py
+++ src/spherinator/models/dense_model.py
@@ -21,6 +21,7 @@ class DenseModel(nn.Module):
         self.example_input_array = torch.randn(1, layer_dims[0])
 
         layers = []
+        layers.append(nn.Flatten())
         for i in range(len(layer_dims) - 1):
             layers.append(nn.Linear(layer_dims[i], layer_dims[i + 1]))
             if i < len(layer_dims) - 2 and activation:
