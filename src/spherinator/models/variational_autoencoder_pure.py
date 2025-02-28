fix reconstruction loss calculation in VariationalAutoencoderPure and add training sampling test

src/spherinator/models/variational_autoencoder_pure.py
@@ -108,7 +108,9 @@ class VariationalAutoencoderPure(pl.LightningModule):
             loss_recon = -torch.log(
                 truncated_normal_distribution(
                     recon, mu=batch, sigma=error, a=0.0, b=1.0
-                ).mean(0)
+                )
+                .flatten(1)
+                .mean(1)
             )
 
         loss_KL = torch.distributions.kl.kl_divergence(q_z, p_z)