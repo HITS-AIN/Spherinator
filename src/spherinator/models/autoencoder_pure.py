2735ac8d12a27ec7f2cb769bc3d272777d1a7cc2
Bernd Doser <bernd.doser@h-its.org>
2025-03-03 16:55:46 +0100

    ONNX export general yaml model

--- src/spherinator/models/autoencoder_pure.py
+++ src/spherinator/models/autoencoder_pure.py
@@ -29,8 +29,8 @@ class AutoencoderPure(pl.LightningModule):
         self.decoder = decoder
         self.loss = loss
 
-        self.example_input_array = self.encoder.example_input_array
-        # self.example_input_array = torch.randn(2, 1, 12)
+        # self.example_input_array = self.encoder.example_input_array
+        self.example_input_array = torch.randn(2, 1, 12)
 
         if loss == "MSE":
             self.reconstruction_loss = nn.MSELoss()
