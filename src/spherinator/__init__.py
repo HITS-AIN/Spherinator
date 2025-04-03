89dbc22ee52823852b3041036d15b6ef8b58e7a3
Bernd Doser <bernd.doser@h-its.org>
2025-04-03 11:38:04 +0200

    add public API exports for callbacks, data, distributions, and models

--- src/spherinator/__init__.py
+++ src/spherinator/__init__.py
@@ -1,3 +1,11 @@
 import importlib.metadata
 
+from spherinator import callbacks, data, distributions, models
+
 __version__ = importlib.metadata.version("spherinator")
+__all__ = [
+    "callbacks",
+    "data",
+    "distributions",
+    "models",
+]
