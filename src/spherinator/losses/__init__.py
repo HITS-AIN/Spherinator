47e2ce2ef69717de969970fceae05ab93cdd943c
Bernd Doser <bernd.doser@h-its.org>
2026-04-21 16:58:58 +0200

    Add SSIMLoss implementation and corresponding tests

--- src/spherinator/losses/__init__.py
+++ src/spherinator/losses/__init__.py
@@ -4,8 +4,10 @@ This module contains the loss functions used in Spherinator.
 
 from .combined_loss import CombinedLoss
 from .perceptional_loss import PerceptualLoss
+from .ssim_loss import SSIMLoss
 
 __all__ = [
     "CombinedLoss",
     "PerceptualLoss",
+    "SSIMLoss",
 ]
