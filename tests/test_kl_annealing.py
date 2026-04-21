5bf6c92ed70f9cac46aa388d49ed08c529cb4623
Bernd Doser <bernd.doser@h-its.org>
2026-04-21 16:09:27 +0200

    Add test for cyclic KL annealing with multiple cycles in test_kl_annealing

--- tests/test_kl_annealing.py
+++ tests/test_kl_annealing.py
@@ -35,3 +35,14 @@ class TestKLAnnealing:
         trainer, module = self._make_trainer_and_module(5)
         cb.on_train_epoch_start(trainer, module)
         assert module.beta == pytest.approx(0.5)
+
+    def test_cyclic_2(self):
+        cb = KLAnnealing(start=0.0, end=1.0e-2, n_epochs=200, n_cycles=4, ratio=1.0)
+
+        epochs = [0, 1, 49, 50, 200]
+        expected_betas = [0.0, 0.0002, 0.0098, 0.0, 0.01]
+
+        for epoch, expected_beta in zip(epochs, expected_betas):
+            trainer, module = self._make_trainer_and_module(epoch)
+            cb.on_train_epoch_start(trainer, module)
+            assert module.beta == pytest.approx(expected_beta)
