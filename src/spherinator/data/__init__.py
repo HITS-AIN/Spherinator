ea2f43eb1201ed85e4146a46458df9c5a42b1d7a
Bernd Doser <bernd.doser@h-its.org>
2026-05-13 11:46:58 +0200

    Remove unused image data modules and related tests

--- src/spherinator/data/__init__.py
+++ src/spherinator/data/__init__.py
@@ -3,24 +3,16 @@ Entry point of the data package used to provide the access to the data.
 """
 
 from .data_module import Column, DataModule, TransformedDataset
-from .images_data_module import ImagesDataModule
-from .images_dataset import ImagesDataset
-from .mnist_data_module import MNISTDataModule
 from .parquet_data_module import ParquetDataModule
 from .parquet_dataset import ParquetDataset
 from .parquet_dataset_sampling import ParquetDatasetSampling
 from .parquet_dataset_with_error import ParquetDatasetWithError
 from .parquet_iterable_data_module import ParquetIterableDataModule
 from .parquet_iterable_dataset import ParquetIterableDataset
-from .preprocessing import MinMaxNormalize, ToTensor
 
 __all__ = [
     "Column",
     "DataModule",
-    "ImagesDataModule",
-    "MinMaxNormalize",
-    "ImagesDataset",
-    "MNISTDataModule",
     "ParquetDataModule",
     "ParquetDataset",
     "ParquetDatasetSampling",
@@ -28,5 +20,4 @@ __all__ = [
     "ParquetIterableDataModule",
     "ParquetIterableDataset",
     "TransformedDataset",
-    "ToTensor",
 ]
